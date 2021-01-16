5243a83ce9270b64be378de4358e73c1ad56c6ac
peena5370 <67957824+peena5370@users.noreply.github.com>
2021-01-16 17:30:50 +0800

    Update SoapTIS.java

--- Java/SoapTIS.java
+++ Java/SoapTIS.java
@@ -1,4 +1,8 @@
-package com.tispackages;
+/*
+* Developed by: peena5370
+* Published date: 8/Jan/2021
+*/
+package com.packages;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -15,38 +19,41 @@ import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 
 import sun.net.www.protocol.http.HttpURLConnection;
-
-public class SoapTIS {
+	/*
+	 * Create a SOAP services with certain parameter such as barcode, route process, customer name, and part number.
+	 * The function of SOAPTIS() is acting like the web service, it will create and xml request and post it to the url and get response request from the web server.
+	 * After process the SOAP servies, it also takes the response result and create an xml file for the result get.
+	 * */
+public class TIS_SOAP {
 	
 	public void SOAPTIS(String process, String custName, String serialNumb, String partNumb) {
-//	public SoapTIS() {
-//		String process = "AOI_T";
-//		String custName = "YQM";		
-//		String serialNumb = "Y200602MY";
-//		String serialNumb = "TESTBARCODE";
-//		String partNumb = "PYQMIP96B-N";
-		String tester = "SG40305008";
-	
 		try {
+//			declare division name, since the division name is same as the customer name, then adding customer name to the division name
 			String divName = custName.toString();
-//			InetAddress ip = InetAddress.getLocalHost();
-//			String tester = ip.getHostName();
-			String url = "http://sgsind0sgptis01/MES-TIS/TIS.asmx?op=OKToTest";
+//			get the local machine address and tester machine name
+			InetAddress ip = InetAddress.getLocalHost();
+//			declare tester as the machine name
+			String tester = ip.getHostName();
+//			set the soap web service url address for perform soap services
+			String url = "SOAP web service url goes here";
 			URL obj = new URL(url);
 			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+//			set request method for the xml soap services same as the format provided on the web service
 			con.setRequestMethod("POST");
+//			set property type same as the details provided from the web service
 			con.setRequestProperty("Content-Type", "application/soap+xml; charset=utf-8");
+//			set the xml POST request data based on the format of writing the soap web service
 			String xmlRequest = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" + 
 					"<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">\r\n" + 
 					"  <soap12:Body>\r\n" + 
-					"    <OKToTest xmlns=\"http://jabil.com/GMS/MES_TIS\">\r\n" + 
+					"    <TestProcess xmlns=\"process url">\r\n" + 
 					"      <CustomerName>" + custName + "</CustomerName>\r\n" + 
 					"      <Division>"+ divName + "</Division>\r\n" + 
 					"      <SerialNumber>" + serialNumb + "</SerialNumber>\r\n" + 
 					"      <AssemblyNumber>"+ partNumb + "</AssemblyNumber>\r\n" + 
 					"      <TesterName>" + tester + "</TesterName>\r\n" + 
 					"      <ProcessStep>" + process + "</ProcessStep>\r\n" + 
-					"    </OKToTest>\r\n" + 
+					"    </TestProcess>\r\n" + 
 					"  </soap12:Body>\r\n" + 
 					"</soap12:Envelope>";
 			con.setDoOutput(true);
@@ -54,7 +61,9 @@ public class SoapTIS {
 			wr.writeBytes(xmlRequest);
 			wr.flush();
 			wr.close();
+//			get response status from the GET request
 			String responseStatus = con.getResponseMessage();
+//			System.out.println(responseStatus);
 			BufferedReader in = new BufferedReader(new InputStreamReader (con.getInputStream()));
 			String inputLine;
 			StringBuffer response = new StringBuffer();
@@ -64,7 +73,7 @@ public class SoapTIS {
 			in.close();
 			
 //			create xml document from the soap response result 
-			String xmlLocation = "D:\\Temp\\AOI_AXI\\Result.xml";
+			String xmlLocation = "src/res/result.xml";
 			Document newXmlDoc = DocumentHelper.parseText(response.toString());
 			OutputXml(newXmlDoc,xmlLocation);				
 		} catch (Exception e) {
@@ -86,6 +95,5 @@ public class SoapTIS {
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
+	}	
 }
